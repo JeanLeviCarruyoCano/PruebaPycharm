c92ed2abb9c6580f68375e5752f14f191c7a82ae
jeancanon686 <levicarruyo@gmail.com>
2025-11-05 21:16:20 -0500

    J:/utm/pycharm/PruebaPycharm

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic.base import TemplateView
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
